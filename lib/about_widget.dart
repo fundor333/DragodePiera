fc2b221cc4a2b4ce6118fb5be7e51515d324673c
Matteo <fundor333@gmail.com>
2019-03-04 20:08:29 +0100

    Add some info about the devs

--- lib/about_widget.dart
+++ lib/about_widget.dart
@@ -1,5 +1,6 @@
 import 'package:drago_de_piera/models/developer.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutWidget extends StatelessWidget {
   @override
@@ -67,14 +68,7 @@ Ciao. Questa é un app che é stata costruita basandosi sugli OpenData del Comun
       ),
     );
 
-    Widget devlist = DevListRow(developers[0]);
-/*    ListView.builder(
-        itemCount: planets.length,
-        itemBuilder: (BuildContext context, int index) {
-          return DevListRow(planets[index]);
-        });*/
-
-
+    Widget devlist = DevListRow(devs[0]);
 
     return Scaffold(
         body: ListView(
@@ -93,16 +87,27 @@ class DevListRow extends StatelessWidget {
 
   final Developer planet;
 
+  _launchURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   Widget build(BuildContext context) {
     final planetThumbnail = new Container(
-      margin: new EdgeInsets.symmetric(vertical: 16.0),
-      alignment: FractionalOffset.centerLeft,
-      child: new Image(
-        image: new AssetImage(planet.image),
-        height: 92.0,
-        width: 92.0,
-      ),
-    );
+        margin: new EdgeInsets.symmetric(vertical: 16.0),
+        alignment: FractionalOffset.centerLeft,
+        child: GestureDetector(
+          onTap: () =>_launchURL("https://github.com/" + planet.github),
+          child: new Image(
+            image: NetworkImage(
+                "https://avatars.githubusercontent.com/" + planet.github),
+            height: 92.0,
+            width: 92.0,
+          ),
+        ));
 
     final planetCardContent = new Container(
       margin: new EdgeInsets.fromLTRB(76.0, 16.0, 16.0, 16.0),
@@ -116,26 +121,13 @@ class DevListRow extends StatelessWidget {
           ),
           new Container(height: 10.0),
           new Text(
-            planet.location,
+            planet.name,
           ),
           new Container(
               margin: new EdgeInsets.symmetric(vertical: 8.0),
               height: 2.0,
               width: 18.0,
               color: new Color(0xff00c6ff)),
-          new Row(
-            children: <Widget>[
-              new Container(width: 8.0),
-              new Text(
-                planet.distance,
-              ),
-              new Container(width: 24.0),
-              new Container(width: 8.0),
-              new Text(
-                planet.gravity,
-              ),
-            ],
-          ),
         ],
       ),
     );
